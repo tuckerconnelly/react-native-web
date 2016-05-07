4d21bc51649a0f862b89d72b8ca4310038fddca3
Tucker Connelly <web@tuckerconnelly.com>
2016-05-07 19:30:05 -0400

    Create a separate index for switching between web/react-native

---
+++ src/ReactNativeWeb.js
@@ -0,0 +1,87 @@
+import React from 'react'
+
+import './apis/PanResponder/injectResponderEventPlugin'
+
+// apis
+import Animated from './apis/Animated'
+import AppRegistry from './apis/AppRegistry'
+import AppState from './apis/AppState'
+import AsyncStorage from './apis/AsyncStorage'
+import Dimensions from './apis/Dimensions'
+import Easing from './apis/Easing'
+import InteractionManager from './apis/InteractionManager'
+import NetInfo from './apis/NetInfo'
+import PanResponder from './apis/PanResponder'
+import PixelRatio from './apis/PixelRatio'
+import Platform from './apis/Platform'
+import StyleSheet from './apis/StyleSheet'
+import UIManager from './apis/UIManager'
+
+// components
+import ActivityIndicator from './components/ActivityIndicator'
+import Image from './components/Image'
+import ListView from './components/ListView'
+import Portal from './components/Portal'
+import ScrollView from './components/ScrollView'
+import Text from './components/Text'
+import TextInput from './components/TextInput'
+import Touchable from './components/Touchable/Touchable'
+import TouchableBounce from './components/Touchable/TouchableBounce'
+import TouchableHighlight from './components/Touchable/TouchableHighlight'
+import TouchableOpacity from './components/Touchable/TouchableOpacity'
+import TouchableWithoutFeedback from './components/Touchable/TouchableWithoutFeedback'
+import View from './components/View'
+
+// modules
+import NativeModules from './modules/NativeModules'
+
+// propTypes
+
+import ColorPropType from './apis/StyleSheet/ColorPropType'
+import EdgeInsetsPropType from './apis/StyleSheet/EdgeInsetsPropType'
+import PointPropType from './apis/StyleSheet/PointPropType'
+
+const ReactNative = {
+  // apis
+  Animated,
+  AppRegistry,
+  AppState,
+  AsyncStorage,
+  Dimensions,
+  Easing,
+  InteractionManager,
+  NetInfo,
+  PanResponder,
+  PixelRatio,
+  Platform,
+  StyleSheet,
+  UIManager,
+
+  // components
+  ActivityIndicator,
+  Image,
+  ListView,
+  Portal,
+  ScrollView,
+  Text,
+  TextInput,
+  Touchable,
+  TouchableBounce,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+
+  // modules
+  NativeModules,
+
+  // propTypes
+  ColorPropType,
+  EdgeInsetsPropType,
+  PointPropType,
+
+  // React
+  ...React
+}
+
+module.exports = ReactNative
