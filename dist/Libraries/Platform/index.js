09c59056332a9c6f83db55b473e9b60999a18020
Tucker Connelly <web@tuckerconnelly.com>
2016-10-12 10:22:57 -0400

    Add Platform.select

--- dist/Libraries/Platform/index.js
+++ dist/Libraries/Platform/index.js
@@ -2,9 +2,16 @@
 
 var _ExecutionEnvironment = require('fbjs/lib/ExecutionEnvironment');
 
+function select(platformCode) {
+  if (typeof platformCode.web === 'function') return platformCode.web();
+
+  return platformCode.web;
+}
+
 var Platform = {
   OS: 'web',
-  userAgent: _ExecutionEnvironment.canUseDOM ? window.navigator.userAgent : ''
+  userAgent: _ExecutionEnvironment.canUseDOM ? window.navigator.userAgent : '',
+  select: select
 };
 
 module.exports = Platform;
--- src/Libraries/Platform/index.js
+++ src/Libraries/Platform/index.js
@@ -1,8 +1,15 @@
 import { canUseDOM } from 'fbjs/lib/ExecutionEnvironment'
 
+function select(platformCode) {
+  if (typeof platformCode.web === 'function') return platformCode.web()
+
+  return platformCode.web
+}
+
 const Platform = {
   OS: 'web',
-  userAgent: canUseDOM ? window.navigator.userAgent : ''
+  userAgent: canUseDOM ? window.navigator.userAgent : '',
+  select,
 }
 
 module.exports = Platform
