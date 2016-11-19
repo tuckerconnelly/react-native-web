fa24f2c77a9c6f6f620d36661cd7357f6944e72d
Tucker Connelly <web@tuckerconnelly.com>
2016-11-18 21:33:53 -0500

    Switch to animated library

--- dist/Libraries/Animated/index.js
+++ dist/Libraries/Animated/index.js
@@ -1,16 +1,22 @@
 'use strict';
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; /**
-                                                                                                                                                                                                                                                                   * Copyright (c) 2016-present, Nicolas Gallagher.
-                                                                                                                                                                                                                                                                   * Copyright (c) 2015-present, Facebook, Inc.
-                                                                                                                                                                                                                                                                   * All rights reserved.
-                                                                                                                                                                                                                                                                   *
-                                                                                                                                                                                                                                                                   * 
-                                                                                                                                                                                                                                                                   */
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var _AnimatedImplementation = require('./AnimatedImplementation');
+var _animated = require('animated');
 
-var _AnimatedImplementation2 = _interopRequireDefault(_AnimatedImplementation);
+var _animated2 = _interopRequireDefault(_animated);
+
+var _debounce = require('lodash/debounce');
+
+var _debounce2 = _interopRequireDefault(_debounce);
+
+var _AppRegistry = require('../AppRegistry');
+
+var _AppRegistry2 = _interopRequireDefault(_AppRegistry);
+
+var _StyleSheet = require('../StyleSheet');
+
+var _StyleSheet2 = _interopRequireDefault(_StyleSheet);
 
 var _Image = require('../Image');
 
@@ -26,8 +32,37 @@ var _View2 = _interopRequireDefault(_View);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-module.exports = _extends({}, _AnimatedImplementation2.default, {
-  View: _AnimatedImplementation2.default.createAnimatedComponent(_View2.default),
-  Text: _AnimatedImplementation2.default.createAnimatedComponent(_Text2.default),
-  Image: _AnimatedImplementation2.default.createAnimatedComponent(_Image2.default)
+function _traverse(internalInstance, visitor) {
+  visitor(internalInstance);
+  if (internalInstance._renderedComponent) {
+    _traverse(internalInstance._renderedComponent, visitor);
+  }
+  if (internalInstance._renderedChildren) {
+    Object.keys(internalInstance._renderedChildren).forEach(function (childKey) {
+      return _traverse(internalInstance._renderedChildren[childKey], visitor);
+    });
+  }
+}
+
+var callOnLayout = (0, _debounce2.default)(function () {
+  if (!_AppRegistry2.default.getAppElement()) return;
+  _traverse(_AppRegistry2.default.getAppElement()._reactInternalInstance, function (_ref) {
+    var _instance = _ref._instance;
+    return _instance && _instance.handleLayout && _instance.handleLayout();
+  });
+}, 100);
+
+_animated2.default.inject.InteractionManager({
+  createInteractionHandle: function createInteractionHandle() {
+    return 0;
+  },
+  clearInteractionHandle: callOnLayout
+});
+
+_animated2.default.inject.FlattenStyle(_StyleSheet2.default.flatten);
+
+module.exports = _extends({}, _animated2.default, {
+  Image: _animated2.default.createAnimatedComponent(_Image2.default),
+  Text: _animated2.default.createAnimatedComponent(_Text2.default),
+  View: _animated2.default.createAnimatedComponent(_View2.default)
 });
--- src/Libraries/Animated/index.js
+++ src/Libraries/Animated/index.js
@@ -1,19 +1,41 @@
-/**
- * Copyright (c) 2016-present, Nicolas Gallagher.
- * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
- *
- * @flow
- */
+import Animated from 'animated'
+import debounce from 'lodash/debounce'
 
-import AnimatedImplementation from './AnimatedImplementation'
+import AppRegistry from '../AppRegistry'
+import StyleSheet from '../StyleSheet'
 import Image from '../Image'
 import Text from '../Text'
 import View from '../Components/View'
 
+function _traverse(internalInstance, visitor) {
+  visitor(internalInstance)
+  if (internalInstance._renderedComponent) {
+    _traverse(internalInstance._renderedComponent, visitor)
+  }
+  if (internalInstance._renderedChildren) {
+    Object.keys(internalInstance._renderedChildren).forEach(
+      childKey => _traverse(internalInstance._renderedChildren[childKey], visitor)
+    )
+  }
+}
+
+const callOnLayout = debounce(() => {
+  if (!AppRegistry.getAppElement()) return
+  _traverse(AppRegistry.getAppElement()._reactInternalInstance, ({ _instance }) =>
+    _instance && _instance.handleLayout && _instance.handleLayout()
+  )
+}, 100)
+
+Animated.inject.InteractionManager({
+  createInteractionHandle: () => 0,
+  clearInteractionHandle: callOnLayout,
+})
+
+Animated.inject.FlattenStyle(StyleSheet.flatten)
+
 module.exports = {
-  ...AnimatedImplementation,
-  View: AnimatedImplementation.createAnimatedComponent(View),
-  Text: AnimatedImplementation.createAnimatedComponent(Text),
-  Image: AnimatedImplementation.createAnimatedComponent(Image)
+  ...Animated,
+  Image: Animated.createAnimatedComponent(Image),
+  Text: Animated.createAnimatedComponent(Text),
+  View: Animated.createAnimatedComponent(View)
 }
